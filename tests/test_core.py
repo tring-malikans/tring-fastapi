0c5a9d517cc08e02fa12142ca494081493fed66e
sufiyank97 <sufiyan7697@gmail.com>
2021-09-15 00:22:13 +0530

    added

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,9 +1,9 @@
 from fastapi.testclient import TestClient
-from app.main import app
+from api.main import app
 
 client = TestClient(app)
 
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+    assert response.json() == {"message": "Hello World"}
