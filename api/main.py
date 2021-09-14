5b413c2be93609adeeedf2bac4b6fa8c67bb0f8b
sufiyank97 <sufiyan7697@gmail.com>
2021-09-15 00:36:57 +0530

    added

--- api/main.py
+++ api/main.py
@@ -8,4 +8,13 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-handler = Mangum(app=app)
+# handler = Mangum(app=app)
+def handler(event, context):
+    if event.get("some-key"):
+        # Do something or return, etc.
+        return
+
+    asgi_handler = Mangum(app)
+    response = asgi_handler(event, context) # Call the instance with the event arguments
+
+    return response
